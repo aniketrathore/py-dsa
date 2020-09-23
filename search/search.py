619e4892f1cb3de0c7d12c7b7a8fad1a22419683
aniketrathore <aniketrathore16@gmail.com>
2020-09-23 20:09:05 +0530

    Search: Add binary search. Fix Documentation.
    Signed-off-by: aniketrathore <aniketrathore16@gmail.com>

--- search/search.py
+++ search/search.py
@@ -1,8 +1,7 @@
-class LinearSearch:
-
+class Search:
     """
-    Linear search work with both sorted and unsorted data.
-    This class provide to interface for searching.
+    This class provide interfaces to search data in
+    sorted or unsorted array.
     """
 
     def __init__(self, items):
@@ -10,7 +9,7 @@ class LinearSearch:
             raise ValueError("Invalid data type.")
         self.items = items
 
-    def search(self, value):
+    def linear_search(self, value):
         """
         Search for given data in linear time.
         Time Complexity: O(n)
@@ -22,9 +21,27 @@ class LinearSearch:
                 return f"{value} found at {i} location."
         return "Item not found."
 
+    def binary_search(self, value):
+        """
+        Search for given data in logarithmic time.
+        Array should be sorted and contain numbers.
+        Time Complexity: O(log n)
+        """
+        items = self.items
+        low, high = 0, len(items) - 1
+        while low <= high:
+            mid = (high + low) // 2
+            if value == items[mid]:
+                return f"{value} found at {mid} location."
+            elif value < items[mid]:
+                high = mid - 1
+            elif value > items[mid]:
+                low = mid + 1
+        return "Item not found."
+
     def fast_search(self, value):
         """
-        Search for given data in linear logarithmic time.
+        Search for given data in logarithmic time.
         Time Complexity: O(log n)
         """
         if value is None:
@@ -39,5 +56,3 @@ class LinearSearch:
             left += 1
             right -= 1
         return "Item not found."
-
-
